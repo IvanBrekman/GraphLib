633fdb2429f80a5c5c47e1b7de62f326b8a7afdb
ivanbrekman <nikitka-zhukov03@mail.ru>
2022-09-15 01:33:24 +0300

    simple sfml

--- main.cpp
+++ main.cpp
@@ -1,7 +1,23 @@
-#include <stdio.h>
+#include <SFML/Graphics.hpp>
 
 int main(void) {
-    printf("Hello World!\n");
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::CircleShape  shape(100.f);
+    
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) {
+                window.close();
+            }
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
 
     return 0;
 }
--- makefile
+++ makefile
@@ -1,7 +1,16 @@
-OUT_FILE=cat.purrrrr
+BIN			= bin
+OUT_FILE	= cat.exe.purrrrr
+OBJ_FILE	= kitty.obj.meow
+
+OBJ			= $(BIN)/$(OBJ_FILE)
+EXE			= $(BIN)/$(OUT_FILE)
+
+SFML_LIBS 	= -lsfml-graphics -lsfml-window -lsfml-system -lsfml-audio -lsfml-network
 
 cr:
 	clear
 
-	g++ main.cpp -o $(OUT_FILE)
-	./$(OUT_FILE)
+	g++ -c main.cpp -o $(OBJ)
+	g++ $(OBJ) -o $(EXE) $(SFML_LIBS)
+
+	./$(EXE)
